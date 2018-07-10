ae082305172d5691409a8bbc4e9156e3adcfe811
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-07-10 18:09:52 -0400

    Start actions and reducers

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,16 @@
+import { merge } from 'lodash';
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+
+const sessionReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+
+    case LOGOUT_CURRENT_USER:
+
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
