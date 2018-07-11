f4a90050689cd8920540c575b4e05f32375fc945
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-07-11 16:43:22 -0400

    Finish boostrapping current user

--- frontend/cloudcast.jsx
+++ frontend/cloudcast.jsx
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { signup, login, logout } from './actions/session_actions';
-
-
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
-  // TESTING
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
+  // TEST!
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
 
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
