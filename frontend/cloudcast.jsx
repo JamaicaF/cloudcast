86b564f3d5a7099b411e9ec205bbfc2981be5a91
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-07-10 22:57:10 -0400

    Update package config and add Store

--- frontend/cloudcast.jsx
+++ frontend/cloudcast.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, login, logout } from './util/session_api_util'
+import configureStore from './store/store';
+
+import { signup, login, logout } from './util/session_api_util';
+
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
   // TESTING
-  window.signup = signup
-  window.login = login
-  window.logout = logout
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
 
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Cloudcast</h1>, root);
