6260fdddf23a1cc1e4a6a139b99c2365ad1b3681
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-07-10 23:08:24 -0400

    Add root and app components, update entry file

--- frontend/cloudcast.jsx
+++ frontend/cloudcast.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 import { signup, login, logout } from './util/session_api_util';
 
@@ -15,5 +16,5 @@ document.addEventListener("DOMContentLoaded", () => {
   window.logout = logout;
 
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Cloudcast</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
