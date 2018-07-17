c7e0ead84b253f188a63b11f4a16b771dcff15b4
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-07-17 15:09:59 -0400

    Add loading state to cast show component

--- frontend/components/casts/cast_show.jsx
+++ frontend/components/casts/cast_show.jsx
@@ -1,28 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class CastShow extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
   componentDidMount(){
-    this.props.fetchCast(this.props.match.params.castId);
+    this.props.fetchCast(this.props.match.params.castId).then(
+      success => this.setState({loading:false})
+    );
   }
 
   render () {
+    if (this.state.loading) return <div></div>;
+
+    let image;
+    if (this.props.cast.castImage) {
+      image = <img src={this.props.cast.castImage} />;
+    } else {
+      image = null;
+    }
+
     return (
       <div>
         <div className="cast-image-large">
-          {this.props.castImg
-            ? <img src={this.props.castImg} />
-            : null
-          }
+
+          {this.image()}
+
         </div>
 
         <li className="cast-text-info">
           <Link className="cast-title"
-            to={`/casts/${this.props.castId}/`}>{this.props.cast.title}</Link>
+            to={`/casts/${this.props.cast.id}/`}>{this.props.cast.title}</Link>
           <br />
 
           <span className="item-el">by</span>
           &nbsp;
-          <span className="cast-author">{this.props.user.username}</span>
+          <span className="cast-author">Username</span>
         </li>
       </div>
     );
