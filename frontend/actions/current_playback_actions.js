012f6cac87c895676d9ac7b7a5cd6d2699a4939c
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-07-19 14:13:23 -0400

    Update playback actions and reducers

--- frontend/actions/current_playback_actions.js
+++ frontend/actions/current_playback_actions.js
@@ -1,9 +1,15 @@
 export const RECEIVE_PLAYBACK_CAST = "RECEIVE_PLAYBACK_CAST";
-
+export const TOGGLE_PLAY_PAUSE = "TOGGLE_PLAY_PAUSE";
 
 export const receivePlaybackCast = (id) => {
   return {
     type: RECEIVE_PLAYBACK_CAST,
-    id
+    playbackId
+  };
+};
+
+export const toggleplaypause = () => {
+  return {
+    type: TOGGLE_PLAY_PAUSE,
   };
 };
--- frontend/reducers/current_playback_reducer.js
+++ frontend/reducers/current_playback_reducer.js
@@ -1,13 +1,23 @@
-import { RECEIVE_PLAYBACK_CAST } from '../actions/current_playback_actions';
+import { RECEIVE_PLAYBACK_CAST, TOGGLE_PLAY_PAUSE } from '../actions/current_playback_actions';
+import { merge } from 'lodash';
 
 const defaultState = {
+  displayPlaybackBar: false,
   playback: false,
-}
+  playbackId: 0
+};
 
 export default (state = defaultState, action) => {
+  Object.freeze(state);
+  const newState = merge({}, state);
   switch (action.type) {
     case RECEIVE_PLAYBACK_CAST:
-      return {playbackId: action.id};
+      newState.playbackId = action.playbackId;
+      newstate.displayPlaybackBar = true;
+      return newState;
+    case TOGGLE_PLAY_PAUSE:
+      newState.playback = !newState.playback;
+      return newState;
     default:
       return state;
   }
