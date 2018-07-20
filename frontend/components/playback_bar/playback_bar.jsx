84a70d6e7589d9f411e4b625995afd55560e6aaa
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-07-19 21:48:33 -0400

    Add more playbar styling and action toggle

--- frontend/components/playback_bar/playback_bar.jsx
+++ frontend/components/playback_bar/playback_bar.jsx
@@ -5,6 +5,7 @@ class PlaybackBar extends React.Component {
   constructor(props) {
     super(props);
     this.audioTag = React.createRef();
+    this.togglePlayPause = this.togglePlayPause.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -21,53 +22,58 @@ class PlaybackBar extends React.Component {
     }
   }
 
+  togglePlayPause() {
+    this.props.togglePlayPause();
+  }
+
   bar() {
     if (this.props.displayPlaybackBar) {
       return (
-        <audio
-          controls
-          src={this.props.castToPlay.castAudio}
-          ref={this.audioTag}>
-            Your browser does not support this audio element.
-          </audio>
+        <div className="playback-bar">
+            <div className="cast-img-playback">
+              <img src={this.props.castToPlay.castImage} />
+            </div>
+
+            <div className="playback-play-icon" onClick={this.togglePlayPause}>
+              {this.props.playback
+                ? <i className="fas fa-pause"></i>
+                : <i className="fas fa-play"></i>
+              }
+            </div>
+
+            <div className="playback-cast-text-info">
+              <Link className="playback-cast-show-title"
+                to={`/casts/${this.props.castToPlay.id}/`}>{this.props.castToPlay.title}</Link>
+              <br />
+              <div className="playback-cast-show-creator">
+                <span className="">by</span>
+                &nbsp;
+                <span>{this.props.castToPlay.castCreator}</span>
+              </div>
+            </div>
+
+          <div className="progress-bar-container">
+            <audio
+              controls
+              src={this.props.castToPlay.castAudio}
+              ref={this.audioTag}>
+                Your browser does not support this audio element.
+              </audio>
+          </div>
+
+          <div className="playback-volume-icon">
+            <i className="fas fa-volume-up"></i>
+          </div>
+        </div>
       );
     }
     return null;
   }
 
   render() {
-
     return (
-      <div className="playback-bar">
-          <div className="cast-img-playback">
-            <img src={this.props.castToPlay.castImage} />
-          </div>
-
-          <div className="playback-play-icon" onClick={this.togglePlayPause}>
-            {this.props.playback
-              ? <i className="fas fa-pause"></i>
-              : <i className="fas fa-play"></i>
-            }
-          </div>
-
-          <div className="playback-cast-text-info">
-            <Link className="playback-cast-show-title"
-              to={`/casts/${this.props.castToPlay.id}/`}>{this.props.castToPlay.title}</Link>
-            <br />
-            <div className="playback-cast-show-creator">
-              <span className="">by</span>
-              &nbsp;
-              <span>{this.props.castToPlay.castCreator}</span>
-            </div>
-          </div>
-          
-        <div className="progress-bar-container">
-          {this.bar()}
-        </div>
-
-        <div className="playback-play-icon">
-          <i className="fas fa-volume-up"></i>
-        </div>
+      <div>
+        {this.bar()}
       </div>
     );
   }
