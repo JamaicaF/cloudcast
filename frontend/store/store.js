244908be1c4a0837aedd515c37dc6b9af304bcc6
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-08-01 13:52:31 -0400

    Destructure development logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,20 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
 
-// let middleware = [thunk];
-// if (process.env.NODE_ENV !== 'production') {
-//   const logger = require('redux-logger');
-//   middleware = [...middleware, logger];
-// }
+let middleware = [thunk];
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middleware.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => {
   return createStore(
     rootReducer,
     preloadedState,
-    // applyMiddleware(...middleware)
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middleware)
   );
 };
 
