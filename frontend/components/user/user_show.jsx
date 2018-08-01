530eab735b5d4e0fd7fb6b26405728db778b67ce
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-07-31 22:45:04 -0400

    Build out user show, temp fix store

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CastIndexItem from '../casts/cast_index_item';
 
 class UserShow extends React.Component {
   constructor(props) {
@@ -26,10 +27,39 @@ class UserShow extends React.Component {
   }
 
   render () {
+    const userCasts = this.props.user.castIds.map((cast) => {
+      debugger
+      return <CastIndexItem key={`cast-${cast.id}`}
+        cast={cast}
+        currentPlayback={this.props.currentPlayback}
+        receivePlaybackCast={this.props.receivePlaybackCast}
+        togglePlayPause={this.props.togglePlayPause}
+        />;
+    })
+
     if (this.state.loading) return <div />;
 
     return (
-      <div className="">
+      <div className="show-content">
+        <div className="show-page-header">
+          <h2 className="document-title">{this.props.user.username}</h2>
+        </div>
+        <div className="show-page-body">
+          {(this.props.user.id === this.props.currentUser
+          ) ? (
+            <div className="cast-creator-actions">
+              <span className="cast-action-title"
+                onClick={this.handleEdit}>Edit Profile</span>
+            </div>
+          ) : null}
+        </div>
+
+        <h2 className="document-title">Shows</h2>
+        <div className="upload-content">
+          <ul>
+            {userCasts}
+          </ul>
+        </div>
       </div>
     )
   }
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,18 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
+import logger from 'redux-logger';
 
-let middleware = [thunk];
-if (process.env.NODE_ENV !== 'production') {
-  const logger = require('redux-logger');
-  middleware = [...middleware, logger];
-}
+// let middleware = [thunk];
+// if (process.env.NODE_ENV !== 'production') {
+//   const logger = require('redux-logger');
+//   middleware = [...middleware, logger];
+// }
 
 const configureStore = (preloadedState = {}) => {
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(...middleware)
+    // applyMiddleware(...middleware)
+    applyMiddleware(thunk, logger)
   );
 };
 
