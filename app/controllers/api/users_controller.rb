dbdadf049c1e79d834fe17e42ac0a794ba006f8c
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-08-01 23:48:23 -0400

    Fix update params in users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,7 +15,7 @@ class Api::UsersController < ApplicationController
   end
 
   def update
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
     if @user.update(user_params)
       render :show
     else
