5894f906122f556a6e65f5f36270c0bbaa63e91a
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-08-08 17:57:36 -0400

    Add default cast image

--- app/models/cast.rb
+++ app/models/cast.rb
@@ -13,7 +13,7 @@
 class Cast < ApplicationRecord
   validates :title, :user_id, presence: true
 
-  validate :ensure_cast_audio
+  before_save :ensure_cast_audio, :ensure_cast_image
 
   belongs_to :user
 
@@ -23,6 +23,12 @@ class Cast < ApplicationRecord
   def ensure_cast_audio
     unless self.cast_audio.attached?
       errors[:cast_audio] << "must be attached"
+    end
+  end
+
+  def ensure_cast_image
+    unless self.cast_image.attached?
+      self.cast_image.attach(io: File.open('app/assets/images/cloud_default.jpg'), filename: 'cloud_default.jpg')
     end
   end
 end
