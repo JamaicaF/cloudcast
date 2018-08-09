1ce823a319346ceac7d6ef89317a852ac2f0287b
JamaicaF <32247746+JamaicaF@users.noreply.github.com>
2018-08-09 10:32:07 -0400

    Add new users to entities.users

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -2,10 +2,13 @@ import { merge } from 'lodash';
 
 import { RECEIVE_USER } from '../actions/user_actions';
 import { RECEIVE_ALL_CASTS } from '../actions/cast_actions';
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, state, { [action.currentUser.id]: action.currentUser});
     case RECEIVE_USER:
       return merge({}, state, { [action.user.id]: action.user });
     case RECEIVE_ALL_CASTS:
